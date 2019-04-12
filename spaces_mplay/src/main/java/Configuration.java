427a5e2f81df85b3211f8ac951e0f91cc8463a84
Thyago Mota <motat@moravian.edu>
2019-04-12 14:55:49 -0400

    mplay

--- spaces_mplay/src/main/java/Configuration.java
+++ spaces_mplay/src/main/java/Configuration.java
@@ -8,6 +8,6 @@
 public class Configuration {
     static final String SPACE_HOST = "localhost";
     static final String SPACE_NAME = "mplay";
-    static final String SPACE_URL  = "jini://" + SPACE_HOST + "/./" + SPACE_NAME;
+    static final String SPACE_URL  = "jini://" + SPACE_HOST + "/./" + SPACE_NAME + "?locators=" + SPACE_HOST;
     static final int    GUI_REFRESH_TIME = 200; // in ms
 }
